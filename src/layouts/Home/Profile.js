3e0e2dc2b64fd5655723949209e9d27a4bf21ec6
SHYAM <shyam_gr@SHYAMs-MacBook-Pro.local>
2023-08-01 04:55:24 +0530

    nwest

--- src/layouts/Home/Profile.js
+++ src/layouts/Home/Profile.js
@@ -21,7 +21,7 @@ const ProfileText = ({ visible, titleId }) => (
       <DecoderText text="Hi there✌️" start={visible} delay={500} />
     </Heading>
     <Text className={styles.description} data-visible={visible} size="l" as="p">
-    Hello! I'm Shyam Sundar, a passionate UI/UX designer  <Link href="https://www.juspay.in">Juspay Technologies</Link> with a focus on creating exceptional digital experiences in the fintech and e-commerce domains.My love for design fuels my desire to create experiences that users will truly love at ❤️. <Link href="https://drive.google.com/file/d/1fNytNbhXkw38Vls0_ssm40YlqzDyqnXC/view?usp=sharing"> My Resume </Link>
+    Hello! I'm Shyam Sundar, a passionate UI/UX designer  <Link href="https://www.juspay.in">Juspay Technologies </Link> with a focus on creating exceptional digital experiences in the fintech and e-commerce domains.My love for design fuels my desire to create experiences that users will truly love at ❤️. <Link href="https://drive.google.com/file/d/1fNytNbhXkw38Vls0_ssm40YlqzDyqnXC/view?usp=sharing"> My Resume </Link>
     </Text>
   </Fragment>
 );
